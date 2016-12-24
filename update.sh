336fd975ba7efc55a1be85ed3b8254d77048e15c
Jason Spohn <atari911@gmail.com>
2016-12-23 21:14:20 -0800

    Add files via upload

---
+++ update.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo apt-get update 
+sudo apt-get -y upgrade 
+sudo apt-get -y autoremove 
+sudo apt-get -y autoclean 
+sudo apt-get -y dist-upgrade 
+sudo apt-get -y autoremove
+sudo apt-get -y autoclean
